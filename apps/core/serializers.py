2837ddd294ca1e58e423f4e0ea38275ee902c812
Marisa Yeung <neurosa@gmail.com>
2019-08-27 23:26:58 -0700

    updating serializers for a better output

--- apps/core/serializers.py
+++ apps/core/serializers.py
@@ -1,32 +1,56 @@
-from django.contrib.auth.models import User
+from apps.accounts.models import User
 from rest_framework import serializers
 from apps.core.models import Habit, Log
 
 
-class LogSerializer(serializers.HyperlinkedModelSerializer):
-    # habit = serializers.ReadOnlyField(source="habit.title")
-
+class LogSerializer(serializers.ModelSerializer):
+    # date = serializers.DateTimeField()
+    # score = serializers.IntegerField()
     class Meta:
         model = Log
-        fields = ['url', 'id', 'date', 'score', 'habit']
+        fields = ['date', 'score']
 
 
-class HabitSerializer(serializers.HyperlinkedModelSerializer):
-    logs = serializers.HyperlinkedRelatedField(many=True, view_name='log-detail', read_only=True)
-    owner = serializers.HyperlinkedRelatedField(view_name='user-detail', read_only=True)
-    # owner = serializers.ReadOnlyField(source='owner.username')
+class HabitSerializer(serializers.ModelSerializer):
+    logs = LogSerializer(many=True)
 
     class Meta:
         model = Habit
-        fields = ['url', 'id', 'owner', 'title', 'display_type', 'weekly_goal', 'logs']
+        fields = ["title", "display_type", "weekly_goal", "logs"]
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    # habits = serializers.HyperlinkedRelatedField(many=True, view_name='habit-detail', read_only=True)
-    # habits = serializers.ReadOnlyField(source='habit.title')
+class UserSerializer(serializers.ModelSerializer):
+    habits = HabitSerializer(many=True)
 
     class Meta:
         model = User
-        fields = ['url', 'id', 'habits']
-        # fields = ['url', 'id']
+        fields = ["habits"]
+
+# class LogSerializer(serializers.HyperlinkedModelSerializer):
+#     # habit = serializers.ReadOnlyField(source="habit.title")
+#
+#     class Meta:
+#         model = Log
+#         fields = ['url', 'id', 'date', 'score', 'habit']
+
+
+# class HabitSerializer(serializers.HyperlinkedModelSerializer):
+#     logs = serializers.HyperlinkedRelatedField(many=True, view_name='log-detail', read_only=True)
+#     owner = serializers.HyperlinkedRelatedField(view_name='user-detail', read_only=True)
+#     # owner = serializers.ReadOnlyField(source='owner.username')
+#
+#     class Meta:
+#         model = Habit
+#         fields = ['url', 'id', 'owner', 'title', 'display_type', 'weekly_goal', 'logs']
+
+
+# class UserSerializer(serializers.HyperlinkedModelSerializer):
+#     habits = serializers.HyperlinkedRelatedField(many=True, view_name='habit-detail', read_only=True)
+#     # logs = LogSerializer()
+#     # habits = serializers.ReadOnlyField(source='habit.title')
+#
+#     class Meta:
+#         model = User
+#         fields = ['habits']
+#         # fields = ['url', 'id']
 
